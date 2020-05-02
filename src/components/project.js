1f2015d41d0cc8861d6e0d9b3e00eac39087a439
Elvis Knapman <elvis@elvisknapman.com>
2020-05-02 16:23:00 -0400

    removed links, added json property data

--- src/components/project.js
+++ src/components/project.js
@@ -6,16 +6,15 @@ const Project = ({ project }) => {
   console.log("PROJECT", project)
   return (
     <>
-      <div class="content-box">
-        <a
-          class="headline-link"
-          href="https://reduxtodo.now.sh/"
-          target="_blank"
-        >
-          <h2 class="feature-heading">{project.node.title}</h2>
-        </a>
-        <a href="https://reduxtodo.now.sh/" target="_blank">
-          <Img fluid={project.node.image.childImageSharp.fluid} />
+      <div className="content-box">
+        <h2 className="feature-heading">{project.node.title}</h2>
+
+        <a href={project.node.project_link} target="_blank">
+          <Img
+            className="project-image"
+            fluid={project.node.image.childImageSharp.fluid}
+            alt={project.node.image_alt_text}
+          />
         </a>
         <p className="project-desc">{project.node.description}</p>
       </div>
