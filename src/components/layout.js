b4ee04e72a5f805dd94d7dc70bb7fbaf700decd8
Elvis Knapman <elvis@elvisknapman.com>
2020-05-02 20:16:20 -0400

    material UI changes
    material UI icon imports and style overrides

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,21 @@
 import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import InsertDriveFileOutlinedIcon from "@material-ui/icons/InsertDriveFileOutlined"
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
+import EmailOutlinedIcon from "@material-ui/icons/EmailOutlined"
 
 import NavLogo from "../data/images/ek-only-nav.png"
 import FooterLogo from "../data/images/ek-logo.png"
 
+const useStyles = makeStyles({
+  root: {
+    fontSize: "1.8rem",
+    marginRight: "0.6rem",
+  },
+})
+
 const Layout = ({ children }) => {
+  const classes = useStyles()
   return (
     <>
       <header>
@@ -13,13 +25,14 @@ const Layout = ({ children }) => {
           </div>
           <div className="right-nav">
             <a href="#learn-more">
-              <i className="far fa-file"></i>Projects
+              <InsertDriveFileOutlinedIcon classes={{ root: classes.root }} />{" "}
+              Projects
             </a>
             <a href="#">
-              <i className="fas fa-question-circle"></i>About
+              <InfoOutlinedIcon classes={{ root: classes.root }} /> About
             </a>
             <a href="#">
-              <i className="fas fa-envelope"></i>Contact
+              <EmailOutlinedIcon classes={{ root: classes.root }} /> Contact
             </a>
           </div>
         </nav>
